1af845c683b71ebe475bcc053af289a5721c96a0
Yang Dai <yangdai713@aim.com>
2018-05-14 15:37:13 -0400

    complete lecture 3

---
+++ lecture_3_Algorithms/sigma_recursion.c
@@ -0,0 +1,33 @@
+// Sums a range of numbers iteratively
+
+
+#include <cs50.h>
+#include <stdio.h>
+
+
+int sigma(int m);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Positive integer: ");
+    }
+    while (n < 1);
+    int answer = sigma(n);
+    printf("%i\n", answer);
+}
+
+// Return sum of 1 through m
+int sigma(int m)
+{
+    if (m <= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return (m + sigma(m - 1));
+    }
+}
