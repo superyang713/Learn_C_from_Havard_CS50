d755b7309dbbb2b5e9fee2723a3ccfa9e1518d5e
Yang Dai <yangdai713@aim.com>
2018-05-23 17:02:44 -0400

    working on c

---
+++ Duke_Coursera_Programming_C/course_3_Pointers_Arrays_Recursion/swap_with_pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int swap(int *x, int *y);
+int main(void)
+{
+    int a = 5;
+    int b = 8;
+    swap(&a, &b);
+    printf("value a: %d, value b: %d\n", a, b);
+    return EXIT_SUCCESS;
+}
+
+int swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
