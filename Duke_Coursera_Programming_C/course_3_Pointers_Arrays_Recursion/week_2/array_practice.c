6ace79059f9ef53188ed41fb738f9359037f920b
Yang Dai <yangdai713@aim.com>
2018-05-23 17:32:36 -0400

    working on week_2

---
+++ Duke_Coursera_Programming_C/course_3_Pointers_Arrays_Recursion/week_2/array_practice.c
@@ -0,0 +1,22 @@
+#include <stdio>
+#include <stdlib>
+
+
+int sumArray(int *array, int n)
+{
+    int answer = 0;
+    int *ptr  = array;
+    for (int i = 0; i < n; i++)
+    {
+        answer += *ptr;
+    }
+    return answer;
+}
+
+int main(void)
+{
+    int data[4] = {4, 6, 8, 3};
+    int sum = sumArray(data, 4);
+    printf("%d\n", sum);
+    return EXIT_SUCCESS;
+}
